bb3331fb4843dfc25aae45541706dbbece5dae27
ErickRabago <164416841+ErickRabago@users.noreply.github.com>
2024-07-03 16:38:36 -0700

    diseño responsive del dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar } from '../components/Dashboard/Navbar';
 import { AsideOptions } from '../components/Dashboard/AsideOptions';
 import { NewTestBtn } from '../components/Dashboard/NewTestBtn';
 
 const Dashboard = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
     <>
       {/* Navbar */}
-      <Navbar />
+      <Navbar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
       {/* Aside */}
       <aside
         id="logo-sidebar"
-        className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0"
+        className={`fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform transform ${
+          sidebarOpen ? 'translate-x-0' : '-translate-x-full'
+        } bg-white border-r border-gray-200 sm:translate-x-0`}
         aria-label="Sidebar"
       >
         <div className="h-full px-3 pb-4 overflow-y-auto bg-gradient-to-b from-custom-MainSky to-emerald-100 flex flex-col">
@@ -33,3 +37,4 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
