a7b2c65782bedfe11a6e1a5573a04bda5b6d8d89
ErickRabago <164416841+ErickRabago@users.noreply.github.com>
2024-07-07 23:48:25 -0700

    Se ajustó las rutas del dashboard para que reciba los props del context y las validaciones de los botones.

--- src/components/Dashboard/Navbar.jsx
+++ src/components/Dashboard/Navbar.jsx
@@ -1,59 +1,68 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from '../../services/UserContext';
 
 export const Navbar = ({ sidebarOpen, setSidebarOpen }) => {
+  const { user } = useContext(UserContext);
+
+  // Verificar si user es null o undefined
+  if (!user) {
+    return (
+      <div className="flex items-center justify-center h-full">
+        <p>Loading user data...</p>
+      </div>
+    );
+  }
+
   return (
-    <>
-      <nav className="fixed top-0 z-50 w-full bg-gray-100 border-b border-gray-100">
-        <div className="px-3 py-3 lg:px-5 lg:pl-3">
-          <div className="flex items-center justify-between">
-            {/* Logo */}
-            <div className="flex items-center justify-start rtl:justify-end">
-              <img className="h-14" src="./pin.svg" alt="logo" />
-            </div>
-            {/* Hamburger menu */}
-            <button
-              className="sm:hidden text-gray-600 hover:text-gray-800"
-              onClick={() => setSidebarOpen(!sidebarOpen)}
+    <nav className="fixed top-0 z-50 w-full bg-gray-100 border-b border-gray-100">
+      <div className="px-3 py-3 lg:px-5 lg:pl-3">
+        <div className="flex items-center justify-between">
+          {/* Logo */}
+          <div className="flex items-center justify-start rtl:justify-end">
+            <img className="h-14" src="./pin.svg" alt="logo" />
+          </div>
+          {/* Hamburger menu */}
+          <button
+            className="sm:hidden text-gray-600 hover:text-gray-800"
+            onClick={() => setSidebarOpen(!sidebarOpen)}
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                ></path>
-              </svg>
-            </button>
-            {/* User Info */}
-            <div className="flex items-center">
-              <div className="flex items-center ms-3">
-                <span className="text-xl mr-4"> Erick Rabago </span>
-                {/* User photo */}
-                <div>
-                  <button
-                    type="button"
-                    className="flex text-sm bg-transparent rounded-full ring-4 ring-custom-MainSky"
-                    aria-expanded="false"
-                  >
-                    <span className="sr-only">Open user menu</span>
-                    <img
-                      className="h-10 w-10 sm:h-12 sm:w-12 md:h-14 md:w-14 bg-cover rounded-full"
-                      src="./examplephoto.jpg"
-                      alt="user photo"
-                    />
-                  </button>
-                </div>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </button>
+          {/* User Info */}
+          <div className="flex items-center">
+            <div className="flex items-center ms-3">
+              <span className="text-xl mr-4"> {user.name} {user.lastname} </span>
+              {/* User photo */}
+              <div>
+                <button
+                  type="button"
+                  className="flex text-sm bg-transparent rounded-full ring-4 ring-custom-MainSky"
+                  aria-expanded="false"
+                >
+                  <img
+                    className="h-10 w-10 sm:h-12 sm:w-12 md:h-14 md:w-14 bg-cover rounded-full"
+                    src={user.photo || './examplephoto.jpg'}
+                    alt="user photo"
+                  />
+                </button>
               </div>
             </div>
           </div>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 };
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Navbar } from '../components/Dashboard/Navbar.jsx';
-import { AsideOptions } from '../components/Dashboard/AsideOptions.jsx';
-import { NewTestBtn } from '../components/Dashboard/NewTestBtn.jsx';
-import  MainContent  from '../components/Dashboard/MainContent.jsx';
+import NewTestBtn from '../components/AsideButtons/NewTestBtn.jsx';
+import MainContent from '../components/Dashboard/MainContent.jsx';
 import StudentTable from './StudentsTable';
+import ExamsBtn from '../components/AsideButtons/ExamsBtn.jsx';
+import StudentsBtn from '../components/AsideButtons/StudentsBtn.jsx';
+import MyVideosBtn from '../components/AsideButtons/MyVideosBtn.jsx';
+import MyExamsBtn from '../components/AsideButtons/MyExamsBtn.jsx';
+import { UserContext } from '../services/UserContext';
 
 const Dashboard = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [view, setView] = useState('welcome'); // Estado para controlar la vista
+  const { user, loading } = useContext(UserContext);
+
+  // Manejo de carga
+  if (loading) {
+    return <div>Loading...</div>; // Muestra un mensaje de carga mientras se obtiene el usuario
+  }
+
+  // Verificar si el usuario está definido antes de acceder a user.role
+  if (!user) {
+    return <div>User not logged in.</div>; // Manejar el caso donde el usuario no está definido
+  }
 
   const renderView = () => {
     switch (view) {
@@ -33,8 +48,18 @@ const Dashboard = () => {
         aria-label="Sidebar"
       >
         <div className="h-full px-3 pb-4 overflow-y-auto bg-gradient-to-b from-custom-MainSky to-emerald-100 flex flex-col">
-          <AsideOptions setView={setView} />
-          <NewTestBtn />
+          {user.role === 'teacher' ? (
+            <>
+              <ExamsBtn />
+              <StudentsBtn />
+              <NewTestBtn />
+            </>
+          ) : user.role === 'student' ? (
+            <>
+              <MyVideosBtn />
+              <MyExamsBtn />
+            </>
+          ) : null}
           <img className="h-auto mt-4 rounded-lg shadow-xl border-4 border-teal-800" src="./asideImage.PNG" alt="Test image" />
         </div>
       </aside>
